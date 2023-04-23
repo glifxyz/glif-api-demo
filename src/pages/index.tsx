a25c19c34a35d4ccbb9a7aaef9255df0018cd4b7
Jamie Dubs <jamie@jamiedubs.com>
2023-04-23 19:10:08 -0400

    Tiny UI tweaksp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,18 +33,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="grid text-center lg:grid-cols-2 lg:text-left mt-16">
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className="group rounded-lg border border-transparent px-5 py-4">
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
             Run Glif
           </h2>
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
-            Oreos: great cookie or greatest cookie?
+            Descriptive text might go here, who knows
           </p>
         </div>
         <button
-          className="py-2 px-3 bg-white text-black border border-black"
+          className="py-2 px-3 bg-white text-black border border-black hover:bg-gray-100"
           onClick={runGlif}
         >
           Run Glif
